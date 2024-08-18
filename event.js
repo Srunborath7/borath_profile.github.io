a4148173c72bdc96753967cd3485b0b8d9f41985
Srunborath7 <165261007+Srunborath7@users.noreply.github.com>
2024-08-18 16:29:45 +0700

    Add files via upload

---
+++ event.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById('menu');
+const menu_icon = document.getElementById('menu_icon');
+
+menu_icon.addEventListener('click', () => {
+    menu.classList.toggle("toggleMenu");
+});
+
